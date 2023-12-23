34d77fd2b0ba7c78937569e01072d8802e7204ee
freya02 <41875020+freya022@users.noreply.github.com>
2023-12-23 20:08:49 +0100

    Add Nitter links to twitter transformed links

--- src/main/kotlin/io/github/freya022/bot/link/Links.kt
+++ src/main/kotlin/io/github/freya022/bot/link/Links.kt
@@ -1,6 +1,6 @@
 package io.github.freya022.bot.link
 
-val urlRegex = Regex("""https://.*\.com\S*""")
+val urlRegex = Regex("""https://.+?\.(?:com|net)\S*""")
 
 typealias SourceMessageID = Long
 typealias RepostedMessageID = Long
--- src/main/kotlin/io/github/freya022/bot/link/TwitterLinkTransformer.kt
+++ src/main/kotlin/io/github/freya022/bot/link/TwitterLinkTransformer.kt
@@ -1,15 +1,21 @@
 package io.github.freya022.bot.link
 
+import dev.minn.jda.ktx.interactions.components.row
+import dev.minn.jda.ktx.messages.into
 import io.github.freya022.botcommands.api.core.service.annotations.BService
+import net.dv8tion.jda.api.interactions.components.buttons.Button
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
 @BService
 data object TwitterLinkTransformer : LinkTransformer {
+    private val replacedHosts = setOf("twitter.com", "x.com", "nitter.net", "vxtwitter.com")
+
     override fun editMessageIfNeededOrNull(builder: MessageCreateBuilder): MessageCreateBuilder? {
+        val nitterUrls = arrayListOf<String>()
         val replaced = urlRegex.replace(builder.content) {
             val httpUrl = it.value.toHttpUrlOrNull() ?: return@replace it.value
-            if (httpUrl.host != "twitter.com" && httpUrl.host != "x.com") return@replace it.value
+            if (httpUrl.host !in replacedHosts) return@replace it.value
 
             httpUrl
                 .newBuilder()
@@ -17,10 +23,19 @@ data object TwitterLinkTransformer : LinkTransformer {
                 .query(null)
                 .fragment(null)
                 .toString()
+                .also { url -> nitterUrls.add(url.replaceFirst("vxtwitter.com", "nitter.net")) }
         }
 
         return when {
-            replaced != builder.content -> builder.also { it.setContent(replaced) }
+            nitterUrls.isNotEmpty() -> builder.also { createBuilder ->
+                createBuilder.setContent(replaced)
+                if (nitterUrls.size == 1) {
+                    createBuilder.addComponents(Button.link(nitterUrls.first(), "See on Nitter").into())
+                } else {
+                    val buttons = nitterUrls.mapIndexed { i, url -> Button.link(url, "See #${i + 1} on Nitter") }
+                    createBuilder.addComponents(buttons.chunked(5) { it.row() })
+                }
+            }
             else -> null
         }
     }
