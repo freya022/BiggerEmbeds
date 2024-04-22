5a712d7db78d1cf83ea93e5d7c98731756bdba47
freya02 <41875020+freya022@users.noreply.github.com>
2024-04-23 00:53:30 +0200

    Disable unnecessary intents

--- src/main/kotlin/io/github/freya022/bot/Bot.kt
+++ src/main/kotlin/io/github/freya022/bot/Bot.kt
@@ -5,6 +5,7 @@ import io.github.freya022.bot.config.Config
 import io.github.freya022.botcommands.api.core.JDAService
 import io.github.freya022.botcommands.api.core.events.BReadyEvent
 import io.github.freya022.botcommands.api.core.service.annotations.BService
+import io.github.freya022.botcommands.api.core.utils.enumSetOf
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.hooks.IEventManager
 import net.dv8tion.jda.api.requests.GatewayIntent
@@ -12,7 +13,11 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag
 
 @BService
 class Bot(private val config: Config) : JDAService() {
-    override val intents: Set<GatewayIntent> = defaultIntents + GatewayIntent.MESSAGE_CONTENT
+    override val intents: Set<GatewayIntent> = enumSetOf(
+        GatewayIntent.DIRECT_MESSAGES /* JDA falsely says both intents are required */,
+        GatewayIntent.GUILD_MESSAGES,
+        GatewayIntent.MESSAGE_CONTENT
+    )
 
     override val cacheFlags: Set<CacheFlag> = emptySet()
 
