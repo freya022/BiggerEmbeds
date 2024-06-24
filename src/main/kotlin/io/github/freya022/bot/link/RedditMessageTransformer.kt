5633c8fd4dcede7d70145904f1a0c257dbe80e9a
freya02 <41875020+freya022@users.noreply.github.com>
2024-06-25 00:49:38 +0200

    Add reddit link button

--- src/main/kotlin/io/github/freya022/bot/link/RedditMessageTransformer.kt
+++ src/main/kotlin/io/github/freya022/bot/link/RedditMessageTransformer.kt
@@ -1,12 +1,15 @@
 package io.github.freya022.bot.link
 
+import dev.minn.jda.ktx.interactions.components.row
 import io.github.freya022.botcommands.api.core.service.annotations.BService
+import net.dv8tion.jda.api.interactions.components.buttons.Button
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
 @BService
 data object RedditMessageTransformer : MessageTransformer {
     override suspend fun processMessage(data: TransformData) {
         val builder = data.builder
+        val urls = arrayListOf<String>()
         val replaced = urlRegex.replace(builder.content) {
             val httpUrl = it.value.toHttpUrlOrNull() ?: return@replace it.value
             if (!httpUrl.host.endsWith("reddit.com")) return@replace it.value
@@ -17,9 +20,25 @@ data object RedditMessageTransformer : MessageTransformer {
                 .query(null)
                 .fragment(null)
                 .toString()
+                .also(urls::add)
         }
 
-        if (replaced != builder.content)
-            builder.setContent(replaced)
+        if (urls.isEmpty()) return
+
+        fun String.asRedditUrl() = replaceFirst("rxddit.com", "reddit.com")
+
+        builder.setContent(replaced)
+        if (urls.size == 1) {
+            builder.addActionRow(
+                Button.link(urls.first().asRedditUrl(), "See on Reddit"),
+            )
+        } else {
+            val buttons = urls.flatMapIndexed { i, url ->
+                listOf(
+                    Button.link(url.asRedditUrl(), "See #${i + 1} on Reddit"),
+                )
+            }
+            builder.addComponents(buttons.chunked(4) { it.row() }.take(5))
+        }
     }
 }
