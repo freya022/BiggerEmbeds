fd9af94798fdeb03956281dbd71767ccc416185f
freya02 <41875020+freya022@users.noreply.github.com>
2026-06-08 12:02:11 +0200

    Twitter embeds: Update replaced hosts list, add Nitter link button

--- src/main/kotlin/io/github/freya022/bot/link/TwitterMessageTransformer.kt
+++ src/main/kotlin/io/github/freya022/bot/link/TwitterMessageTransformer.kt
@@ -3,22 +3,29 @@ package io.github.freya022.bot.link
 import dev.freya02.botcommands.jda.ktx.components.row
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import net.dv8tion.jda.api.components.buttons.Button
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.HttpUrl.Companion.toHttpUrl
 
 @BService
 data object TwitterMessageTransformer : MessageTransformer {
-    private const val targetHost = "fxtwitter.com"
-    private val replacedHosts = setOf("twitter.com", "x.com", "nitter.net", "vxtwitter.com", "fxtwitter.com", "fixupx.com")
+    private const val TARGET_HOST = "fxtwitter.com"
+
+    private val urlRegex = run {
+        val replacedHosts = listOf(
+            "twitter.com", "x.com",
+            "nitter.net",
+            "fxtwitter.com", "fixupx.com",
+            "vxtwitter.com", "fixvx.com",
+        )
+        val quotedHosts = replacedHosts.joinToString("|") { Regex.escape(it) }
+        Regex("""https://(?:${quotedHosts})\S*""")
+    }
 
     override suspend fun processMessage(data: TransformData) {
         val urls = arrayListOf<String>()
         val replaced = urlRegex.replace(data.content) {
-            val httpUrl = it.value.toHttpUrlOrNull() ?: return@replace it.value
-            if (httpUrl.host !in replacedHosts) return@replace it.value
-
-            httpUrl
+            it.value.toHttpUrl()
                 .newBuilder()
-                .host(targetHost)
+                .host(TARGET_HOST)
                 .query(null)
                 .fragment(null)
                 .toString()
@@ -27,15 +34,17 @@ data object TwitterMessageTransformer : MessageTransformer {
 
         if (urls.isEmpty()) return
 
-        fun String.asTwitterUrl() = replaceFirst(targetHost, "twitter.com")
-        fun String.asXUrl() = replaceFirst(targetHost, "x.com")
+        fun String.asTwitterUrl() = replaceFirst(TARGET_HOST, "twitter.com")
+        fun String.asXUrl() = replaceFirst(TARGET_HOST, "x.com")
+        fun String.asNitterUrl() = replaceFirst(TARGET_HOST, "nitter.net")
 
         data.setContent(replaced)
         if (urls.size == 1) {
             data.addComponents(
                 row(
-                    Button.link(urls.first().asTwitterUrl(), "See on Twitter"),
-                    Button.link(urls.first().asXUrl(), "See on X"),
+                    Button.link(urls.first().asTwitterUrl(), "Twitter"),
+                    Button.link(urls.first().asXUrl(), "X"),
+                    Button.link(urls.first().asNitterUrl(), "Nitter"),
                 )
             )
         } else {
@@ -43,6 +52,7 @@ data object TwitterMessageTransformer : MessageTransformer {
                 listOf(
                     Button.link(url.asTwitterUrl(), "See #${i + 1} on Twitter"),
                     Button.link(url.asXUrl(), "See #${i + 1} on X"),
+                    Button.link(url.asNitterUrl(), "See #${i + 1} on Nitter"),
                 )
             }
             data.addComponents(buttons.chunked(4) { it.row() }.take(5))
